81e5527ea339f434f584bdd6986f9580a307d8f1
Shehan-Amarasinghe <Shehan.nivantha124@gmail.com>
2024-05-23 12:02:58 +0530

    Update product.jsx file

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Product() {
-  return (
+
+  const [carts, setCarts] = useState([]);
+
+  const fetchCat = async() => {
+    const responsee = await fetch("https://fakestoreapi.com/carts")
+    const resultt = await responsee.json();
+    setCarts(resultt);
+  }
+
+  useEffect(() => {
+    fetchCat();
+  },[])
+
+  return ( 
+    <div>
     <h1>Product</h1>
+    {carts.map((cart) => {
+      return(
+          <ul>
+            <li>{cart.id}</li>
+            <br />
+            <h4>{cart.date}</h4>
+          </ul>
+      )})}
+   </div>
   )
 }
 
